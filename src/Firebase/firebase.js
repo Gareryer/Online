f1aab219303d2b4d24ddfa53e4f38d6bd4ae175b
omprakash599 <84883925+omprakash599@users.noreply.github.com>
2024-05-20 17:16:42 +0530

    test

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyC5UbIuKW07J5zV4i9o1P_0q1WqZ3OWigk",
+    authDomain: "onlinefreewatch-e6d43.firebaseapp.com",
+    projectId: "onlinefreewatch-e6d43",
+    storageBucket: "onlinefreewatch-e6d43.appspot.com",
+    messagingSenderId: "307535060958",
+    appId: "1:307535060958:web:812807040912848032074f",
+    measurementId: "G-NL9V68RQJ9"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
